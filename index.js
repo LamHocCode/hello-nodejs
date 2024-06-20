900f661d480f23e96d336b380b4e065c8420ad9a
LamHocCode <dilamoffical@gmail.com>
2024-06-20 13:57:59 +0700

    Integrated Jenkins with Github

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/jenkins", (res) => {
+    res.send("Tích hợp vào github bằng webhooks")
+}   
+
+)
+
 app.listen(3001, function () {
   console.log("Example app listening on port 3001!");
 });
